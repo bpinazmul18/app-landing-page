29fb83d284a6a0206a71bd4bd8936ce550911f9e
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-11-22 14:18:03 +0600

    add jackpot section and sticky nav

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".js--services-section").waypoint(function(direction){
+        if(direction == "down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    });
+})
