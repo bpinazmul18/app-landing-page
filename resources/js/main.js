5a6e4aa2c506c4cb73a4acf6dd99429e01f8fea3
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-11-23 13:05:08 +0600

    add pricing section main design and factionality

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,4 +6,33 @@ $(document).ready(function(){
             $("nav").removeClass("sticky");
         }
     });
+
+    check();
 })
+
+function check(){
+    var checkBox = document.getElementById("checkBox");
+    var element = document.getElementById("month");
+    var annaul = document.getElementById("annaul");
+    var text1 = document.getElementsByClassName("card-text-1");
+    var text2 = document.getElementsByClassName("card-text-2");
+
+    if ( checkBox.checked == false ) {
+        element.classList.add("switch-color");
+        annaul.classList.remove("switch-color");
+    } else {
+        element.classList.remove("switch-color");
+        annaul.classList.add("switch-color");
+    }
+
+    for (var i = 0; i < text1.length; i++){
+        if(checkBox.checked == false){
+            text1[i].style.display = "block";
+            text2[i].style.display = "none";
+        }
+        else if (checkBox.checked == true){
+            text1[i].style.display = "none";
+            text2[i].style.display = "block"
+        }
+    }
+}
