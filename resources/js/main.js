ce461b8b0fefb158c5ef83c64e9917adf97105a5
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-11-24 20:56:46 +0600

    add mobile first design

--- resources/js/main.js
+++ resources/js/main.js
@@ -71,3 +71,12 @@ function accordion() {
         });
     }
 }
+
+//MOBILE MENU//
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = "0%";
+}
